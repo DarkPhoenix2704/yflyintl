58ae7cab4370e893100dafcc79673d0e4f26b6f2
DarkPhoenix2704 <anbarasun123@gmail.com>
2023-10-18 19:42:38 +0530

    feat: google analytics

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,10 +1,24 @@
 import { Html, Head, Main, NextScript } from "next/document";
+import Script from "next/script";
 
 export default function Document() {
   return (
     <Html lang="en" className="scroll-smooth">
       <Head>
         <link rel="icon" href="/favicon.png" />
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-RRH8BTTPES"
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){window.dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', 'G-RRH8BTTPES');
+        `}
+        </Script>
       </Head>
       <body>
         <Main />
