0697f59447807a29ee693ece202edbb043483a97
DarkPhoenix2704 <anbarasun123@gmail.com>
2023-11-10 19:11:26 +0530

    fix: descriptions

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -12,6 +12,12 @@ export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
       <title>YFLY International Study Abroad</title>
+      <meta property="og:image" content="/seo-og.svg" />
+      <meta
+        name="description"
+        content="Let's Elevate Your Higher Education Journey Together, Unlock New Heights with Our Expert Consultations!"
+      />
+
       <div className={inter.className}>
         <Navbar />
         <Component {...pageProps} />
